15ec4d66f38521937962d18be1909232a84f5d15
Kadu Vieira <kadu012@gmail.com>
2022-12-30 14:57:22 -0300

    adiciona section banner e inicia section pizzas

---
+++ assets/js/banner.js
@@ -0,0 +1,13 @@
+$(".banner").slick({
+  // centerMode: true,
+  slidesToShow: 1,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 8000,
+  // adaptiveHeight: true,
+  /* fade 
+  fade: true,
+  cssEase: 'linear'
+  fade*/
+
+})
